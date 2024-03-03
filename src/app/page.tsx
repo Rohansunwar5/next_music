b8a9601f6b06545813ac744efb71c48ebf62cb06
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-03-04 01:01:29 +0530

    Update  HeroSection.tsx, and tailwind.config.ts.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/80 antialiased bg-grid-white/[0.23]">
-      <h1 className="text-2xl  text-center">
-        Learning Next js 
-      </h1>
+    <main className="min-h-screen bg-black/[0.9] antialiased bg-grid-white/[0.23]">
+      <HeroSection/>
+      <FeaturedCourses />
     </main>
   );
 }
