44deac264695f1a8cd477aa1e9b29e06e1e7a81f
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-03-07 21:05:14 +0530

    Delete Courses.tsx, modify FeaturedCourses.tsx

--- src/app/(root)/(routes)/courses/page.tsx
+++ src/app/(root)/(routes)/courses/page.tsx
@@ -1,17 +1,13 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
-import MusicTestimonialCards from "@/components/TestimonialCards";
-import UpcomingWebinars from "@/components/UpcomingWebinars";
-import WhyChooseUs from "@/components/WhyChooseUs";
+
 
 export default function Home() {
   return (
     <main className=" min-h-screen overflow-y-hidden bg-black/[0.9] antialiased ">
       <HeroSection/>
       <FeaturedCourses />
-      <WhyChooseUs />
-      <MusicTestimonialCards/>
-      <UpcomingWebinars />
+      
     </main>
   );
 }   
